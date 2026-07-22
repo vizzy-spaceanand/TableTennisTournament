0cde3cdd92c614b9a2f254b6d0c2a283d6d713cd
vizzy-spaceanand <vizzy-spaceanand@users.noreply.github.com>
2026-07-22 09:40:02 +0000

    Deploying to gh-pages from @ vizzy-spaceanand/TableTennisTournament@33ebe59a3d8c6b9cb9a0efc3d2735495f93105ce 🚀

--- staging/main.dart.js
+++ staging/main.dart.js
@@ -29586,7 +29586,7 @@ $S:2}
 A.azs.prototype={
 $1(a){var s=A.dc().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/a10d8ac38de835021c8d2f920dbf50a920ccc030/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/69c8c61792f04cc809dfef0c910414fb9afc06cd/":s)+a},
 $S:70}
 A.L6.prototype={
 ZN(){var s=this.agx(),r=$.bl.bN().ImageFilter.MakeColorFilter(s,null)
